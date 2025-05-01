5485b105cab04da4fb2baa5abd89670edb6d14a3
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-05-01 09:42:28 -0700

    update docs

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,21 @@
+# rapr 1.0.0 (development)
+* Major `get_rap()` interface updates
+ - Added `source` argument to toggle between RAP 30m (`"rap-30m"`; Landsat) and RAP 10m (`"rap-10m"`; Sentinel 2) products
+   - For details on new RAP 10m products see: http://rangeland.ntsg.umt.edu/data/rangeland-s2/README and `citation("rapr")`
+ - Added `template` argument for setting target grid for projection of result. 
+   - Default behavior (`template=NULL`) will return a SpatRaster in the native grid system of requested RAP source.
+   - For a large area of interest (that spans multiple UTM zones and `source="rap-10m"`) standard "EPSG:5070" grid system will be used when `template` is not set by the user
+ - **Breaking changes**:
+    - SpatRaster objects passed as `x` are now used as `template` so that the output conforms with the input grid system. To avoid this behavior either specify `template` or convert the SpatRaster to a polygon extent with `terra::as.polygons(ext=TRUE)` (or similar)
+    - Specification of the `product` and `years` arguments is now _required_ (no defaults are set in function definition). This is to encourage users to review the new data sources available, and consider what years and products they want to download.
+* Simplified vignette: replaced knitr and rmarkdown with litedown
+* Simplified unit testing suite: replaced testthat with tinytest
+
+# rapr 0.1.2 (2024-10-24)
+* First tagged GitHub release
+* Check that output directory exists before writing output to path specified in `filename (#3)
+
 # rapr 0.1.1
 * Updated default arguments to `get_rap()`
-* Added a new vignette titled "Accessing Rangeland Analysis Platform (RAP) Gridded Data with R"
+* Added a new vignette titled "Accessing Rangeland Analysis Platform (RAP) Data with R"
 * Added a `NEWS.md` file to track changes to the package.
--- R/get_rap.R
+++ R/get_rap.R
@@ -1,47 +1,93 @@
 #' Get Rangeland Analysis Platform (RAP) Grids
 #'
+#' Two sets of Rangeland Analysis Platform products are available (see `source`
+#' argument). `"rap-30m"` is Landsat-derived and has approximately 30 meter
+#' resolution in WGS84 decimal degrees (`"EPSG:4326"`). This is the data source
+#' that has been used in the rapr package since 2022. A newer source (2025),
+#' `"rap-10m"`, is Sentinel 2-derived and has 10 meter resolution in the local
+#' WGS84 UTM zone (`"EPSG:326XX"`, where XX is the two digit UTM zone number).
+#' See Details for the products and bands available for the different
+#' resolutions and source data.
+#'
 #' @param x Target extent. Derived from an sf, terra, raster or sp object or
-#'   numeric vector containing xmin, ymax, xmax, ymin in WGS84
-#'   longitude/latitude decimal degrees (EPSG:4326).
-#' @param years integer. Year(s) to query
-#' @param product Target data: `"vegetation-biomass"` and/or
-#'   `"vegetation-cover"` (for `"rap-30m"`) and `"pft"` (plant functional type
-#'   cover), `"gap"` (canopy gap), `"arte"` (Artemisia spp. cover), `"iag"`
-#'   (invasive annual grass cover), `"pj"` (pinyon juniper cover))
-#' @param filename Output filename (optional; default stores in temporary files,
-#'   see `terra::sources()`)
-#' @param ... Additional arguments passed to internal RAP query function and
+#'   numeric vector containing `xmin`, `ymax`, `xmax`, `ymin` in WGS84 decimal
+#'   degrees (longitude/latitude, `"EPSG:4326"`).
+#' @param years _integer_. Year(s) to query. Products are available from 1986
+#'   (`source="rap-30m"`) or 2018 (`source="rap-10m"`) up to the year prior to the current
+#'   year, based on availability of the Landsat and Sentinel 2 source data.
+#' @param product Target data: `"vegetation-biomass"`, `"vegetation-cover"`,
+#'   and/or `"vegetation-npp"` for `source="rap-30m"`; `"pft"` (plant functional
+#'   type cover), `"gap"` (canopy gap), `"arte"` (Artemisia spp. cover), `"iag"`
+#'   (invasive annual grass cover), or `"pj"` (pinyon juniper cover) for
+#'   `source="rap-10m"`.
+#' @param filename Output filename (optional; default stores in temporary file
+#'   or in memory, see `terra::tmpFiles()`)
+#' @param ... Additional arguments passed to internal query function and
 #'   `[terra::writeRaster()]`
 #' @param source Grid sources. Options include `"rap-30m"` (default; Landsat)
-#'   and `"rap-10m"` (Sentinel 2).
-#' @param version Target version: `"v3"` and/or `"v2"` (for `"rap-30m`). Ignored
-#'   for `"rap-10m"`.
+#' and `"rap-10m"` (Sentinel 2).
+#' @param version Target version: `"v3"` and/or `"v2"` (for `"rap-30m`).
+#'   Currently ignored for `source="rap-10m"`.
 #' @param legacy _logical_. Use legacy (gdal_translate) method? Default: `TRUE`
 #'   (applies only to `source="rap-30m"`).
 #' @param progress logical. Show progress bar? Default: missing (`NULL`) will
 #'   use progress bar when three or more layers are requested.
-#' @details You can query annual biomass and cover (versions 2 and 3) from 1986
-#'   to present
+#' @details
 #'
-#'   - `product = "vegetation-biomass"` returns two layers per year:
-#'     - `"annual forb and grass"`, `"perennial forb and grass"` (**lbs / acre**)
+#' ## Sources, Products, and Band Information
 #'
-#'   - `product = "vegetation-cover"` returns six layers per year:
-#'     - `"annual forb and grass"`, `"bare ground"`, `"litter"`, `"perennial forb and grass"`, `"shrub"`, `"tree"` (**% cover**)
+#' For `"rap-30m"` you can query several Landsat derived annual biomass,
+#' cover, and Net Primary Productivity products from 1986 to present:
 #'
-#'   When a `filename` argument is not specified, unique temporary files will be
-#'   generated. The resulting SpatRaster object will retain reference to these
-#'   files, and you can remove them manually with
-#'   `unlink(terra::sources(<SpatRaster))`.
+#'   - `product = "vegetation-biomass"` returns [two layers](http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-biomass/v3/README) per year:
 #'
-#'   When a `filename` _is_ specified, temporary files will be removed after the
-#'   result (often a multi- year/layer/product) SpatRaster is written to new
-#'   file.
+#'     - 2 Bands:`"annual forb and grass"`, `"perennial forb and grass"` (**lbs / acre**)
+#'
+#'   - `product = "vegetation-cover"` returns [six layers](http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-cover/v3/README) per year:
+#'
+#'     - 6 Bands: `"annual forb and grass"`, `"bare ground"`, `"litter"`, `"perennial forb and grass"`, `"shrub"`, `"tree"` (**% cover**)
+#'
+#'   - `product = "vegetation-npp"` returns [four layers](http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-npp/v3/README) per year:
+#'
+#'     - 4 Bands: `"annual forb and grass"`, `"perennial forb and grass"`, `"shrub"`, `"tree"` (NPP; kg*C/m^2)
+#'
+#' For `"rap-10m"` you can query several [Sentinel 2 derived cover
+#' products](http://rangeland.ntsg.umt.edu/data/rangeland-s2/README) at 10 meter
+#' resolution from 2018 to present:
+#'
+#'    - `product = "pft"` returns fractional cover estimates of plant functional types:
+#'
+#'      - 6 Bands: `"annual forb and grass"`, `"bare ground"`, `"litter"`, `"perennial forb and grass"`, `"shrub"`, `"tree"`
+#'
+#'    - `product = "gap"` returns canopy gap estimates for four canopy gap size classes:
+#'
+#'      - 4 Bands: `"Gaps 25-50 cm"`, `"Gaps 51-100 cm"`, `"Gaps 100-200 cm"`, `"Gaps >200 cm"`
+#'
+#'    - `product = "arte"` returns cover estimates of Artemisia species, including A. arbuscula, A. cana, A. nova, A. tridentata, and A. tripartita.
+#'
+#'      - 1 Band: `"Artemisia spp."`
+#'
+#'    - `product = "iag"` returns fractional cover estimates of Bromus tectorum, B. arvensis, B. rubens, B. hordeaceus, Eremopyrum triticeum, Schismus spp., Taeniatherum caput-medusae, and Ventenata dubia.
+#'
+#'      - 1 Band: `"invasive annual grass"`
+#'
+#'    - `product = "pj"` returns fractional cover estimates of Juniperus monosperma, J. occidentalis, J. osteosperma, J. scopulorum, Pinus edulis, and P. monophylla.
+#'
+#'      - 1 Band: `"pinyon-juniper"`
+#'
+#' ## Temporary Files
+#'
+#'   Large requests may generate intermediate objects that will be stored as
+#'   temporary files. See [terra::tmpFiles()] to view the file paths. These
+#'   files will be removed when an **R** session ends.
+#'
+#' ## Alternate Specification of Area of Interest
 #'
 #'   In lieu of a spatial object from \{terra\}, \{raster\}, \{sf\} or \{sp\}
 #'   packages you may specify a bounding box using a numeric vector containing
-#'   `xmin`, `ymax`, `xmax`, `ymin` in WGS84 longitude/latitude decimal degrees
-#'   (corresponding to order used in `gdal_translate` `-projwin` option). e.g.
+#'   the top-left and bottom-right coordinates (`xmin`, `ymax`, `xmax`, `ymin`)
+#'   in WGS84 longitude/latitude decimal degrees. This corresponds to the
+#'   conventional order used in the `gdal_translate` `-projwin` option. e.g.
 #'   `get_rap(x = c(-120, 37, -119.99, 36.99), ...)`.
 #'
 #' ```
@@ -52,10 +98,23 @@
 #'         |                                   |
 #'         |---------------------------(3: xmax, 4: ymin)
 #' ```
-#' @return a SpatRaster containing the requested product layers by year.
-#'   Native cell resolution of `"rap-30m"` is ~30m x 30m in WGS84 geographic coordinate system.
-#'   Native cell resolution of `"rap-10m"` is 10m x 10m in WGS84 Universal Transverse Mercator (UTM) zone.
-
+#' ## Native Resolution and Projection Systems
+#'
+#' Native cell resolution of `"rap-30m"` is approximately 30m x 30m in WGS84
+#' geographic coordinate system (longitude, latitude). Native cell resolution of
+#' `"rap-10m"` is 10m x 10m in the local (projected) WGS84 Universal Transverse
+#' Mercator (UTM) system.
+#'
+#' For `"rap-10m"` requests spanning _multiple_ UTM zones, either pass a
+#' _SpatRaster_ object as `x` or specify `template` argument. In lieu of a
+#' user-specified grid system for multi-zone requests, a default CONUS Albers
+#' Equal Area projection (`"EPSG:5070"`) with 10 m resolution will be used.
+#'
+#' @return a _SpatRaster_ containing the requested product layers by year.
+#'
+#' @references See `citation("rapr")` for all references related to Rangeland
+#'   Analysis Platform products.
+#'
 #' @export
 get_rap <- function(x,
                     years,
@@ -68,27 +127,27 @@ get_rap <- function(x,
                     progress = NULL) {
 
   source <- match.arg(tolower(source), choices = c("rap-30m", "rap-10m"))
-  
+
   if (source == "rap-10m") {
     # RAP 10m through new interface
-    
+
     # version currently ignored for RAP 10m data
     product <- match.arg(tolower(product), choices = c("pft", "gap", "arte", "iag", "pj"), several.ok = TRUE)
-    
+
     # TODO: implement progress bar? verbose argument is similar but provides more info
     .get_rap_internal(
-      x, 
-      years = years, 
-      product = product, 
-      filename = filename, 
+      x,
+      years = years,
+      product = product,
+      filename = filename,
       ...
     )
   } else if (source == "rap-30m" && isTRUE(legacy)) {
     # RAP 30m through old interface
     version <- match.arg(tolower(version), choices = c("v3", "v2"), several.ok = TRUE)
-    
-    product <- match.arg(tolower(product), choices = c("vegetation-biomass", "vegetation-cover"), several.ok = TRUE)
-    
+
+    product <- match.arg(tolower(product), choices = c("vegetation-biomass", "vegetation-cover", "vegetation-npp"), several.ok = TRUE)
+
     .get_rap_30m_legacy(
       x,
       years = years,
@@ -101,5 +160,5 @@ get_rap <- function(x,
     # RAP 30m through new interface
     stop("RAP 30m data can only be accessed through the legacy interface at this time. Please set `legacy=TRUE`", call. = FALSE)
   }
-  
+
 }
